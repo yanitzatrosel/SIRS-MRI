35c8c7afbebf4c12cb11981f7d5e16ebbcdb3b00
yanitzatrosel <141436347+yanitzatrosel@users.noreply.github.com>
2023-09-11 22:39:59 -0400

    Update CharagundlaMagPLOT.m

--- CharagundlaMagPLOT.m
+++ CharagundlaMagPLOT.m
@@ -1,27 +1,28 @@
 % Define the range of x-values
-TSL = linspace(0, 1, 100); % You can adjust the range and number of points
+f = linspace(0.9 , 1, 100); % You can adjust the range and number of points
 
 % Define values (adjust as needed)
 gamma = 267.513e6; % Gyromagnetic ratio, proton (rad/s/T)
 P = 1; %spin-lock amplitude Bsl=P*B1
-tau = 1e-6; % nominal pi/2 pulse width
+tau = 1e-6; % nominal pi/2 pulse width in seconds
 %TSL = 200e-3; %Spin-Lock time in miliseconds
-T1p = 0.95; % Longitudinal relaxation in seconds. Gray matter
+T1p = 0.1; % Longitudinal relaxation in seconds. Gray matter
 T2p = 0.1;  % transverse relaxation in seconds. Gray matter
+B1 = 1.25e3*6.28; % maximun rf amplitude in rad/s
 
 % Define the values of B1
-B1_values = [20, 40, 60, 80, 100];
+TSL_values = [0.2, 0.4, 0.6, 0.8, 1.0];
 
 
-% Iterate through each value of b
-for i = 1:length(B1_values)
-    B1 = B1_values(i);
+% Iterate through each value of TSL
+for i = 1:length(TSL_values)
+    TSL = TSL_values(i);
     
     % Calculate Mz
-    Mz = (sin(gamma*B1*tau).^2).*exp(-TSL/T1p)+(cos(gamma*B1*tau).^2).*cos(gamma*B1*tau).*exp(-TSL/T2p);
+    Mz = ((sin(gamma*B1*tau*f)).^2).*exp(-TSL/T1p)+(((cos(gamma*B1*tau*f)).^2).*cos(P*gamma*B1*TSL*f).*exp(-TSL/(T2p)));
     
     % Plot the line for the current value of b
-    plot(TSL, Mz, 'LineWidth', 2);
+    plot(f, Mz, 'LineWidth', 2);
     
     % Hold on to add more lines to the same plot
     hold on;
@@ -32,11 +33,11 @@ end
 %Mz2 = exp(-TSL/T1p);
 
 % Add labels and legend
-xlabel('TSL (s)');
+xlabel('fraction of B1 (Hz)');
 ylabel('Mz');
 title('Longitudinal magnetization');
 grid on;
-legend('B1=20', 'B1=40', 'B1=60', 'B1=80', 'B1=100');
+legend('TSL=0.2', 'TSL=0.4', 'TSL=0.6', 'TSL=0.8', 'TSL=1.0');
 
 % Turn off the hold mode
 hold off;
