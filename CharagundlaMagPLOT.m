92b4a4d25eebda4f1db657bac9636fbbba80daf5
yanitzatrosel <141436347+yanitzatrosel@users.noreply.github.com>
2023-09-21 16:43:59 -0400

    Update CharagundlaMagPLOT.m

--- CharagundlaMagPLOT.m
+++ CharagundlaMagPLOT.m
@@ -1,28 +1,36 @@
-% Define the range of x-values
-f = linspace(0.9 , 1, 100); % You can adjust the range and number of points
+close all; clear all;
 
 % Define values (adjust as needed)
-gamma = 267.513e6; % Gyromagnetic ratio, proton (rad/s/T)
+gamma = 42.58*1e6; % Hz/T % 267.513e6; % Gyromagnetic ratio, proton (rad/s/T)
 P = 1; %spin-lock amplitude Bsl=P*B1
 tau = 1e-6; % nominal pi/2 pulse width in seconds
-%TSL = 200e-3; %Spin-Lock time in miliseconds
 T1p = 0.1; % Longitudinal relaxation in seconds. Gray matter
 T2p = 0.1;  % transverse relaxation in seconds. Gray matter
-B1 = 1.25e3*6.28; % maximun rf amplitude in rad/s
+%B1 = 1.25e3.*6.28; % maximun rf amplitude in rad/s
+
+B1_percentage=[90:0.00001:100];
+B1=(B1_percentage./100);
+
+theta = (gamma*B1*tau.*1.25e3)./(gamma.*gamma);
+%theta = gamma*B1*tau.*1.25e3;
+
+alpha = P*gamma*B1.*1.25e3./gamma;
+%alpha = P*gamma*B1.*1.25e3;
 
 % Define the values of B1
 TSL_values = [0.2, 0.4, 0.6, 0.8, 1.0];
-
+%TSL_values = [1.6, 1.6, 1.6, 1.6, 1.6];
 
 % Iterate through each value of TSL
 for i = 1:length(TSL_values)
     TSL = TSL_values(i);
     
     % Calculate Mz
-    Mz = ((sin(gamma*B1*tau*f)).^2).*exp(-TSL/T1p)+(((cos(gamma*B1*tau*f)).^2).*cos(P*gamma*B1*TSL*f).*exp(-TSL/(T2p)));
+    Mz = sin(theta).*sin(theta).*exp(-TSL/T1p)+cos(theta).*cos(theta).*cos(alpha.*TSL).*exp(-TSL/T2p);
     
     % Plot the line for the current value of b
-    plot(f, Mz, 'LineWidth', 2);
+    plot(B1, Mz, 'LineWidth', 1);
+    ylim([-0.25 0.25])
     
     % Hold on to add more lines to the same plot
     hold on;
